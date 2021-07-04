699d305e38ef4cd0cd5cb7f4cdb05cb93f2487e6
osdifera <neezcafe@gmail.com>
2021-07-03 18:00:32 -0700

    chore: cleaning up code

--- services/ethplorer_service.go
+++ services/ethplorer_service.go
@@ -14,13 +14,8 @@ func GetTopHolders(address string) []models.Holder {
 
 	var(
 		listOfholders models.ListOfHolders
-		//holders []models.Holder
 	)
 	
-
-	//var LiqResponse LiqResponse
-	//json.Unmarshal(bodyBytes, &LiqResponse)
-
 	endpointURL := "https://api.ethplorer.io/getTopTokenHolders/"+address+"?apiKey=EK-xy1hm-wXaD35q-oNbjf&limit=15"
 	resp, err := http.Get(endpointURL)
 	if err != nil {
@@ -29,15 +24,10 @@ func GetTopHolders(address string) []models.Holder {
 
 	defer resp.Body.Close()
 	bodyBytes, _ := ioutil.ReadAll(resp.Body)
-	//sb := string(bodyBytes)
 	
 	if err := json.Unmarshal([]byte(bodyBytes), &listOfholders); err != nil {
   		fmt.Println("failed to unmarshal:", err)
-	} else {
-		fmt.Println(listOfholders.Holders) //
 	}
 
-   	//log.Printf(sb)
-	//fmt.Printf("Holders : %+v", holders)
 	return listOfholders.Holders
 }
